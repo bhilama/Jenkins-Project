93f9fd979ed6466cf13ff28eeb7e62fcf3313e21
bhilama <infy.maheshb@gmail.com>
2025-01-05 16:48:42 -0500

    First Commit.

---
+++ src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,18 @@
+package mavenforjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UITest {
+
+	@Test
+	public void startBrowser() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
+	}
+}
