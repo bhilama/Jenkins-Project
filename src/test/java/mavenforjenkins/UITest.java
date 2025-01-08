94691e84e5f2b233e1faa6abe66f76e57c9c38e4
bhilama <infy.maheshb@gmail.com>
2025-01-07 19:56:59 -0500

    Parameterized

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -3,12 +3,17 @@ package mavenforjenkins;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class UITest {
 
+	@Parameters("Browser")
 	@Test
-	public void startBrowser() {
+	public void startBrowser(String browserName) {
+		
+		System.out.println("Browser type is: " + browserName);
+		
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
